db58473b0ddb488a4b4d4654958a8b6a39463b46
Nathan Furnal <nathan_furnal@hotmail.com>
2021-12-02 14:10:08 +0100

    Try something in Racket

--- day1/day1.rkt
+++ day1/day1.rkt
@@ -1,9 +1,24 @@
-#lang racket
+#lang racket/base
 
-(call-with-input-file "input1.txt"
+(define path "input1.txt")
+
+(call-with-input-file path
   (lambda (in-port)
     (let loop ()
       (let ([data (read-line in-port)])
         (unless (eof-object? data)
           (displayln data)
           (loop))))))
+
+
+(call-with-input-file path
+  (lambda (in-port)
+    (let loop ()
+      (let ([curr (string->number (read-line in-port))]
+            [next (string->number (peek-string 1 0 in-port))])
+        (unless (eof-object? next)
+          (cond
+            [(> next curr) (display curr)])
+          (loop))))))
+
+
