9036d0640b6a740cc7392d11c5b220544fcca85c
Nathan Furnal <nathan_furnal@hotmail.com>
2021-12-06 14:53:23 +0100

    Cleanup

--- day6/day6.py
+++ day6/day6.py
@@ -6,13 +6,16 @@ with open(path, 'r') as f:
     data = list(map(int, f.readline().strip().split(",")))
 
 
-def n_fishes(data: list[int], n_days: int, n_states=9):
-    states = [0] * n_states
+def n_fishes(data: list[int], n_days: int, n_states: int = 9) -> int:
+    states = [0] * n_states  # [0:8] possible values
     for val in data:
-        states[val] += 1
+        states[val] += 1  # set the initial states
     for _ in range(n_days):
+        # get the number of zeros for a given day
         nb_zeros = states[0]
+        # shift every value left and make states[0] -> states[-1]
         states = states[1:] + [nb_zeros]
+        # Spawn new fishes any time zeros are shifted left (they start at value 6)
         states[6] += nb_zeros
     return sum(states)
 
