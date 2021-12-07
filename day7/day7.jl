39238bc976ff0eef482c42200966389e263c9da8
Nathan Furnal <nathan_furnal@hotmail.com>
2021-12-07 20:03:50 +0100

    change return types to avoid conversion

--- day7/day7.jl
+++ day7/day7.jl
@@ -4,14 +4,14 @@ data = parse.(Int, split(readline(joinpath(@__DIR__, "input.txt")), ","))
 
 # Part 1
 
-cost1(arr, val) = sum(abs.(arr .- val))
-println(cost1(data, convert(Int, median(data))))
+cost1(arr, val):: Int = sum(abs.(arr .- val))
+println(cost1(data, median(data)))
 
 # Part 2
 
 gauss_sum(dist) = dist .* (dist .+ 1) ./ 2
-cost2(arr, val) = sum(gauss_sum(abs.(arr .- val)))
+cost2(arr, val) :: Int = sum(gauss_sum(abs.(arr .- val)))
 
 c1 = cost2(data, round(mean(data) - 1/2))
 c2 = cost2(data, round(mean(data) + 1/2))
-println(convert(Int, min(c1, c2)))
+println(min(c1, c2))
